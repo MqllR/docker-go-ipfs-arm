5a58d93417857bb4be1a8b8559b0cbb3b87d4773
MqllR <mael.regnery@protonmail.com>
2018-12-09 13:17:20 +0000

    feat: pass docker command to ipfs daemon

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+if test -z $IPFS_PATH
+then
+  echo '$IPFS_PATH need to declared'
+  exit 1
+fi
+
 if ! test -d $IPFS_PATH
 then
-  mkdir -p $IPFS_PATH && ipfs init
+  mkdir -p $IPFS_PATH
+  ipfs init
 fi
 
 test -n $IPFS_STORAGE_MAX && \
@@ -11,4 +18,9 @@ test -n $IPFS_STORAGE_MAX && \
 test -n $IPFS_ANNOUNCE && \
   sed -i "s#^\(.*\"Announce\).*#\1\": $IPFS_ANNOUNCE,#" $IPFS_PATH/config
 
-ipfs daemon
+if test $# -gt 0
+then
+  ipfs daemon $@
+else
+  ipfs daemon
+fi
